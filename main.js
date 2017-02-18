c2b5aabed0ad4f393b8a63a5bdbd1e463b3eba56
Kofi Yamoah <kyamoah7@gmail.com>
2017-02-18 09:16:34 -0500

    Second Commit for Revsions of Lab8 now Lab9

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+//Second Commit
+var Cart = document.getElementById("basket");
+var CartTotal = document.getElementById("basket");
+var groceries ={ 
+things: [
+				{item:"Chicken", price:4.65},
+        {item:"Tomato",price:.68},
+        {item:"Pasta",price:3.68},
+        {item:"cheese",price:2.99},
+],
+total: function(){
+var sum = 0;
+for (var i = 0; i<=this.things.length-1; i++){
+sum = this.things[i].price +sum}
+return sum;
+}
+	
+}
+for (var i = 0; i<=groceries.things.length-1; i++) {
+ Cart.innerHTML+= (groceries.things[i].item + " : " + groceries.things[i].price ) + "<br>";
+  //console.log(groceries.things[i].item + " : " + groceries.things[i].price);
+};
+document.body.appendChild(Cart);
+CartTotal.innerHTML += groceries.total();
+console.log(groceries.total());
+document.body.appendChild(CartTotal);
+//groceries.total();
+/*
+//First Commit
+var CartTotal = document.createElement('div');
 var groceries ={ 
 things: [
 				{item:"Chicken", price:4.65},
@@ -14,6 +44,6 @@ console.log(sum);
 	
 }
 for (var i = 0; i<=groceries.things.length-1; i++) {
-  console.log(groceries.things[i].item + " : " + groceries.things[i].price);
+   console.log(groceries.things[i].item + " : " + groceries.things[i].price);
 };
-groceries.total();
+groceries.total();*/
